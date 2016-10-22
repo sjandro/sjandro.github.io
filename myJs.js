94cbc6cf47c86ea99cabfca982f8668b66d1d9ab
Alex Salazar <sjandro16@gmail.com>
2016-10-22 16:13:07 -0700

    website responsive update 6

--- myJs.js
+++ myJs.js
@@ -1,2 +1,10 @@
 $(document).ready(function(){
+    // $(".social_icon").click(function() {
+    //   var productLink = $(this).find("a");
+    //
+    //   productLink.attr("target", "_blank");
+    //   window.open(productLink.attr("https://www.linkedin.com/in/salazar6"));
+    //
+    //   return false;
+    // });
 });
